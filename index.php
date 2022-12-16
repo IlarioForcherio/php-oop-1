a295031846e42833b6f9407b424592ee05669096
ilarioforcherio <ilarioforcherio@gmail.com>
2022-12-16 15:04:18 +0100

    constructor + metodo

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php 
+
+include_once __DIR__  . '/classi/movie.php';
+
+
+$movie1 = new Movie( 'batman', 1990, 'action', 9  );
+echo $movie1->voteRange();
+var_dump($movie1);
+
+$movie2 = new Movie( 'spiderman', 2000, 'action', 6  );
+echo $movie2->voteRange();
+var_dump($movie2);
+
+$movie3 = new Movie ('monella', 1998, 'erotico', 4);
+echo $movie3->voteRange();
+var_dump($movie3);
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>php-oop</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <header>
+    <!-- place navbar here -->
+  </header>
+  <main>
+
+  </main>
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
+
